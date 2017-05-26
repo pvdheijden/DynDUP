4638d1ff4d0d235e2a176959e0a18f631c28aaba
Pim <pim@zifzaf.com>
2017-05-26 14:40:17 +0200

    script update

--- update-dns-arecord.sh
+++ update-dns-arecord.sh
@@ -2,7 +2,7 @@
 set -e
 
 API_URL=https://9zoxbqmw3m.execute-api.eu-west-1.amazonaws.com/prod/DnsARecord
-API_KEY=9AIoM6ypFE3USfbt167Yq9VEMMty4Qmq1Crq7Jnc
+API_KEY={{api_key}}
 
 curl -i -X POST $API_URL\
     --header "Content-Type: application/json"   \
