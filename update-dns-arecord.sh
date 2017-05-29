98f764463f6919a18ed578e160b28a91ba498a02
Pim <pim@zifzaf.com>
2017-05-29 11:32:04 +0200

    env based API_KEY

--- update-dns-arecord.sh
+++ update-dns-arecord.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
+#API_KEY= # Make available via environment variable
 API_URL=https://9zoxbqmw3m.execute-api.eu-west-1.amazonaws.com/prod/DnsARecord
-API_KEY={{api_key}}
+RECORD_DATA=record-data.json
 
 curl -i -X POST $API_URL\
     --header "Content-Type: application/json"   \
     --header "x-api-key: $API_KEY"              \
-    --data "@record-data.json"
+    --data "@$RECORD_DATA"
